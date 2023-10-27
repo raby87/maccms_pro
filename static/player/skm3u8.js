1515d92d8783cbabc8d82819c81e5fa9daea3161
faulk <181962585@qq.com>
2023-10-27 11:39:05 +0800

    速看资源的播放器引入

---
+++ static/player/skm3u8.js
@@ -0,0 +1,2 @@
+MacPlayer.Html = '<iframe width="100%" height="'+MacPlayer.Height+'" src="https://open.wn231.com/?url='+MacPlayer.PlayUrl+'" frameborder="0" border="0" marginwidth="0" marginheight="0" scrolling="no" allowfullscreen="true"></iframe>';
+MacPlayer.Show();
